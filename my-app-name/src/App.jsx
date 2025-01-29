89b7e6e814ef486c29ddd9ccc6d9a2e77c3a9a38
Kelly Horak <khorak@uw.edu>
2025-01-29 09:56:27 -0800

    set up firebase

---
+++ my-app-name/src/App.jsx
@@ -0,0 +1,4 @@
+//import the function from the realtime database module
+import { getDatabase } from 'firebase/database';
+// Get a reference to the database service
+const db = getDatabase();
---
+++ my-app-name/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSpEDImVxQ1g6YUy8U60WqxjiW8FDOlvY",
+  authDomain: "clearwaters-1507b.firebaseapp.com",
+  projectId: "clearwaters-1507b",
+  storageBucket: "clearwaters-1507b.firebasestorage.app",
+  messagingSenderId: "533013616938",
+  appId: "1:533013616938:web:ab2efa200361d726412653"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
