06464dbd5c51ab6e4dcfdaefd651b592198b1eca
Sophia Mangrubang <mangrsop@uw.edu>
2025-02-20 15:54:50 -0800

    nav error?

--- ClearWAters/src/navbar.jsx
+++ ClearWAters/src/navbar.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
+import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
         <header>
+            <li>testing</li>
+            {/* 
+                <li id='logo'>
+                        <Link to="/homepage" aria-label="Go to homepage">
+                        </Link>
+                </li>
+                <li id='home-link'>
+                        <Link to="/homepage" aria-label="Go to homepage">   
+                        </Link>
+                </li>
+                <li id='map-link'>
+                        <Link to="/map" aria-label="Map">Home</Link>
+                </li>
+                 <li id='resources-link'>
+                        <Link to="/resources" aria-label="Resources">Leaderboard</Link>
+                 </li>
+             <img src="img/logo.png" alt="Home" />
+            */} 
             <nav>
-                <li> Hi! </li>
+                <ul>
+                    <li>
+                        <Link to="/" aria-label="Home">Home</Link>
+                    </li>
+                </ul>
             </nav>
         </header>
     );
