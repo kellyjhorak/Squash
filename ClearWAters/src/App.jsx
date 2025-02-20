d378e60af05b36eff1b9b270309c516ce05d82da
Kelly Horak <khorak@uw.edu>
2025-02-20 15:16:11 -0800

    made navbar file

--- ClearWAters/src/App.jsx
+++ ClearWAters/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from "react-router-dom"
+import Navbar from './navbar';
 
 const App = () => {
   return (
-    <div>App update</div>
-  )
-}
+    <>
+      <div>App update</div>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ ClearWAters/src/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav>
+                <li> Hi! </li>
+            </nav>
+        </header>
+    );
+}
