3b3329a287813aa943c7eaa87e66c197e19186f5
Kelly Horak <khorak@uw.edu>
2025-02-20 16:09:01 -0800

    add nav bar styling

--- ClearWAters/src/navbar.jsx
+++ ClearWAters/src/navbar.jsx
@@ -2,30 +2,31 @@ import React from "react";
 import App from './App';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './styling.css';
 
 import { Link } from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <header>
-            <nav>
+      <header>
+        <nav>
+          <ul> {/* Wrap the list items in a <ul> */}
             <li id='logo'>
-                        <Link to="/homepage" aria-label="Go to homepage">
-                            <img src="img/logo.png" alt="Logo" />
-                        </Link>
-                </li>
-                <li id='home-link'>
-                        <Link to="/homepage" aria-label="Go to homepage">   
-                        </Link>
-                </li>
-                <li id='map-link'>
-                        <Link to="/map" aria-label="Map">Home</Link>
-                </li>
-                 <li id='resources-link'>
-                        <Link to="/resources" aria-label="Resources">Leaderboard</Link>
-                 </li>
-             <img src="img/logo.png" alt="Home" />
-            </nav>       
-        </header>
+              <Link to="/homepage" aria-label="Go to homepage">
+                <img src="img/logo.png" alt="Logo" />
+              </Link>
+            </li>
+            <li id='home-link'>
+              <Link to="/homepage" aria-label="Go to homepage">Home</Link>
+            </li>
+            <li id='map-link'>
+              <Link to="/map" aria-label="Map">Map</Link>
+            </li>
+            <li id='resources-link'>
+              <Link to="/resources" aria-label="Resources">Leaderboard</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
-}
+  }
