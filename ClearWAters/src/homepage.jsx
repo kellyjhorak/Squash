46d895628de0b266a775573d567d9fc3b9f080b3
Reem Abdulhameed <reem.alhusseini05@gmail.com>
2025-02-25 15:56:51 -0800

    homepage.jsx fixed

--- ClearWAters/src/homepage.jsx
+++ ClearWAters/src/homepage.jsx
@@ -1,3 +1,14 @@
 {/* this will be the landing page where users type in their zipcode*/} 
 
-
+ import React from 'react';
+ import Navbar from './navbar';
+ import Footer from './Footer';
+ 
+ export default function Homepage() {
+     return (
+       <div>
+         <Navbar />
+         <Footer />
+       </div>
+     );
+   }
