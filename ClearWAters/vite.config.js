49601a763b1565347301c76d99231cde68303315
Razan Mansour <rtm2023@uw.edu>
2025-03-01 21:55:10 -0800

    url is now public using serveo server

--- ClearWAters/vite.config.js
+++ ClearWAters/vite.config.js
@@ -1,7 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['clearwaters.serveo.net']
+    // Alternatively, allow any Serveo domain with:
+    // allowedHosts: ['.serveo.net']
+  }
 })
+server: {
+  allowedHosts: ['.serveo.net']
+}
