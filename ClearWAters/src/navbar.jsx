0ff85dadabb56543518a3b0fd8249d909c694b7b
Sophia Mangrubang <mangrsop@uw.edu>
2025-03-09 15:22:52 -0700

    added hamburger menu that functions on tablet/mobile

--- ClearWAters/src/navbar.jsx
+++ ClearWAters/src/navbar.jsx
@@ -5,19 +5,54 @@ import { BrowserRouter } from 'react-router-dom';
 import './styling.css';
 import exampleLogo from './images/logo_large.png';
 import hamburger from './images/hamburger_icon.png';
-
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
+
 export default function Navbar() {
-    return (
-      <header>
-        <nav id="full-menu">
-          <ul> {/* Wrap the listt items in a <ul> */}
-            <li id='logo'>
-              <Link to="/homepage" aria-label="Go to homepage">
-              <img src={exampleLogo} alt="Clearwaters logo" />
-              </Link>
-            </li>
+
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  return (
+    <header>
+      <nav id="full-menu">
+        <ul>
+          <li id='logo'>
+            <Link to="/homepage" aria-label="Go to homepage">
+            <img src={exampleLogo} alt="Clearwaters logo" />
+            </Link>
+          </li>
+          <li id='home-link'>
+            <Link to="/homepage" aria-label="Go to homepage">Home</Link>
+          </li>
+          <li id='map-link'>
+            <Link to="/map" aria-label="Map">Map</Link>
+          </li>
+          <li id='resources-link'>
+            <Link to="/resources" aria-label="Resources">Resources</Link>
+          </li>
+        </ul>
+      </nav>
+      
+      <div> 
+        <div id="mini-menu-bar">
+            <Link to="/homepage" aria-label="Go to homepage">
+                <img id="mini-logo" src={exampleLogo} alt="Clearwaters logo" />
+            </Link>
+            <img id="hamburger-button"
+              className={isMenuVisible ? 'hidden' : ''}
+              src={hamburger}
+              alt="hamburger menu icon"
+              onClick={toggleMenu}
+            />
+        </div>
+    
+        <nav id="hamburger-menu" className={isMenuVisible ? 'visible' : 'hidden'}>
+          <ul>
             <li id='home-link'>
               <Link to="/homepage" aria-label="Go to homepage">Home</Link>
             </li>
@@ -29,14 +64,8 @@ export default function Navbar() {
             </li>
           </ul>
         </nav>
-        <img id="hamburger-button" class="hidden" src={hamburger} alt="hambuger menu icon"/>
-        <nav id="hamburger-menu" class="hidden">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
+      </div>
+      
+    </header>
+  );
   }
